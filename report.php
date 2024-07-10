d32bc8d1d94f02ec0dd8413e342959f2095b2359
loswan <149003443+loswan@users.noreply.github.com>
2024-07-10 07:01:51 +0700

    Update report.php

--- report.php
+++ report.php
@@ -1,43 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 23, 2023 at 08:08 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+<?php
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+$conn = mysqli_connect("localhost","root","","db");
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+$message = "test ya";
+$curl = curl_init();
 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.fonnte.com/send',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('target' => '083162336745','message' => $message),
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: __J#kheaYPV4yJf+eyRE'
+  ),
+));
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+$response = curl_exec($curl);
 
---
--- Database: `hargamo1_tutorial`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `report`
---
-
-CREATE TABLE `report` (
-  `id` int(11) NOT NULL,
-  `device` varchar(255) DEFAULT NULL,
-  `target` varchar(255) NOT NULL,
-  `message` text NOT NULL,
-  `stateid` varchar(255) DEFAULT NULL,
-  `status` varchar(255) NOT NULL,
-  `state` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+curl_close($curl);
+$res = json_decode($response,true);
+var_dump($res);
+foreach($res["id"] as $k=>$v){
+  $target = $res["target"][$k];
+  $status = $res["process"];
+  mysqli_query($conn,"INSERT INTO report (id,target,message,status) VALUES ('$v','$target','$message','$status')");
+}
